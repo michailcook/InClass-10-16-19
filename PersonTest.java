f0594bdd811e33ca7f18ecf4674b87ff9a0a8485
Michail Cook <michailcook@gmail.com>
2019-10-16 14:02:50 -0500

    Initial Commit from cloned code

--- PersonTest.java
+++ PersonTest.java
@@ -1,71 +1,17 @@
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-public class PersonTest {
+class PersonTest {
 
-	@Before
-	public void setUp() throws Exception {
+	@BeforeEach
+	void setUp() throws Exception {
 	}
 
 	@Test
-	public void test() {
-		try {
-			String firstName = "";
-			String lastName = "Smith";
-			Person x = new Person(firstName, lastName);
-			fail("Where is my exception?");
-		} catch (NameException e) {
-
-		}
-	}
-	
-	@Test
-	public void constructorGoodTest() {
-		try {
-			String firstName = "Bo";
-			String lastName = "Smith";
-			Person x = new Person(firstName, lastName);
-			
-		} catch (NameException e) {
-			fail("Where is my exception?");
-		}
-	}
-	
-	@Test
-	public void describeTest() {
-		try {
-			String firstName = "Bo";
-			String lastName = "Smith";
-			Person x = new Person(firstName, lastName);
-			
-			String expected = "*** First Name: " + firstName 
-					+ " Last Name: " + lastName + "***";
-			
-			String actual = x.describeSelf();
-			assertEquals(expected, actual);
-			
-		} catch (NameException e) {
-			fail("Where is my exception?");
-		}
-	}
-	@Test
-	public void constructorOrderTest() {
-		try {
-			String firstName = "Bo";
-			String lastName = "Smith";
-			Person x = new Person(firstName, lastName);
-			
-			String expected = "*** First Name: " + firstName 
-					+ " Last Name: " + lastName + "***";
-			
-			String actual = x.describeSelf();
-			assertEquals(expected, actual);
-			
-		} catch (NameException e) {
-			fail("Where is my exception?");
-		}
+	void test() {
+		fail("Not yet implemented");
 	}
 
 }
