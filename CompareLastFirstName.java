ec389f93a16c9e18fff88604314176c68549dedd
Michail Cook <michailcook@gmail.com>
2019-10-16 14:35:49 -0500

    Last and First name compare method

--- CompareLastFirstName.java
+++ CompareLastFirstName.java
@@ -1,4 +1,42 @@
+import java.util.Comparator;
 
-public class CompareLastFirstName {
+public class CompareLastFirstName implements Comparator<Student>{
 
-}
+	
+	public int CompareLastFirstName(Student first, Student last) {
+		String firstName = first.getFirstName();
+		String lastName = first.getLastName();
+		String firstNameLast = last.getFirstName();
+		String lastNameLast = last.getLastName();
+		if(firstName.compareTo(firstNameLast) < 0) {
+			return -1;
+		}
+		
+		else if(firstName.compareTo(firstNameLast) == 0) {
+			if(lastName.compareTo(lastNameLast) < 0) {
+				return -1;
+			}
+			else if(lastName.compareTo(lastNameLast) == 0) {
+			
+			}
+			else {
+				return -1;
+				
+			}
+		}
+	
+	
+		else {
+			return 1;
+		
+	}
+	}
+	
+
+	@Override
+	public int compare(Student arg0, Student arg1) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	}
+
